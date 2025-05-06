549f11742478e9989a871a68588ff120ed981ffd
NITHISH P <nithishp.22it@kongu.edu>
2025-05-06 23:04:26 +0530

    Update Login.js

--- social_media_dashboard_analyzer/src/components/Login.js
+++ social_media_dashboard_analyzer/src/components/Login.js
@@ -12,7 +12,7 @@ function Login({ setIsAuthenticated }) {
     setError(null); // Reset any previous error
 
     try {
-      const response = await fetch('http://localhost:5002/api/auth/login', {
+      const response = await fetch('http://localhost:5001/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
